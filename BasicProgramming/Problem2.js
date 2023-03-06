e988e50a64e5771fb8a0094bdd6a536fdd199164
Ashraf <ashrafrahman716@gmail.com>
2023-03-06 20:22:18 +0530

    sum

--- BasicProgramming/Problem2.js
+++ BasicProgramming/Problem2.js
@@ -1,5 +1,33 @@
+//adding two number
 function sum(num,nub){
     console.log(num+nub)
 
 }
-sum(2,3)
+// sum(2,3)
+
+
+//Minus 
+function minus(num,nub){
+    console.log(num-nub)
+
+}
+// minus(2,3)
+
+//divide
+function divide(num,num){
+    console.log(num/num)
+}
+// divide(3,3)
+ 
+
+//Modulo
+function Modulo(num,num){
+    console.log(num%num===0)
+}
+// Modulo(3,3)
+
+//Multiple by 3 number
+function Multiple(num,num){
+    console.log(num*num)
+}
+Multiple(3,3)
